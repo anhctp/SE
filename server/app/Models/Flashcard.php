67971a89d2497bafa5d85d85b09ff9f401577de9
Thuy <qingshui2003@gmail.com>
2023-05-02 00:56:18 +0700

    create database

---
+++ server/app/Models/Flashcard.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Flashcard extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'front',
+        'back',
+    ];
+}
---
+++ server/database/migrations/2023_05_01_174614_create_grammars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grammars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lesson_id')->references('id')->on('lessons');
+            $table->string('structure', 255);
+            $table->text('explanation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grammars');
+    }
+};
