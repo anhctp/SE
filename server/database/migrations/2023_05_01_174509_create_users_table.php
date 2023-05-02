00d122101a3f15f8ea68a456a758f51a6bfea559
Thuy <qingshui2003@gmail.com>
2023-05-02 09:23:22 +0700

    fix table users

--- server/database/migrations/2023_05_01_174509_create_users_table.php
+++ server/database/migrations/2023_05_01_174509_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('user_name', 50);
-            $table->string('email', 100);
-            $table->string('password', 50);
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
