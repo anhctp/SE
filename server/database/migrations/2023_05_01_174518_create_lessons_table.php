af8506d07bcf207b75fca766d183dac4e8997830
Thuy <qingshui2003@gmail.com>
2023-05-02 11:12:24 +0700

    insert data into db

--- server/database/migrations/2023_05_01_174518_create_lessons_table.php
+++ server/database/migrations/2023_05_01_174518_create_lessons_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
             $table->string('title', 255);
-            $table->text('description');
             $table->timestamps();
         });
     }
--- server/database/migrations/2023_05_01_174614_create_grammars_table.php
+++ server/database/migrations/2023_05_01_174614_create_grammars_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('lesson_id')->references('id')->on('lessons');
             $table->string('structure', 255);
             $table->text('explanation');
+            $table->text('example');
             $table->timestamps();
         });
     }
